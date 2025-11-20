30644323a7dad0b1191393127c0e3b73c0c7a366
Agne Chomiciute <agne.chomiciute@mif.stud.vu.lt>
2025-11-20 15:52:57 +0200

    sukurtas galutinio balo skaiciavimo testas

--- test/test_calcFinal.cpp
+++ test/test_calcFinal.cpp
@@ -4,7 +4,21 @@
 #include <vector>
 
 
+TEST_CASE("calcFinalGrade correctly computes final grade (mean and median)", "[Student]") {
+    
+    Student s("Test", "Tester");
 
-TEST_CASE("Sanity check") {
-    REQUIRE(1 == 1);
+    // hw grades
+    s.addGrade(7);
+    s.addGrade(8);
+    s.addGrade(9);
+
+    // exam grade
+    s.setExam(10);
+
+    s = calcFinalGrade(s);
+
+    
+    REQUIRE(s.finalMean() == Approx(9.2));
+    REQUIRE(s.finalMedian() == Approx(9.2));
 }
